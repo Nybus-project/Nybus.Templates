85263abc67840792b86f592fd5da70280b0f59ec
Renato Golia <golia.renato@gmail.com>
2019-02-13 23:25:38 +0100

    Add RabbitMQ Windows Service on .NET Core (#4)

---
+++ content/NetCoreRabbitMqWindowsService/Handlers/TestCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Nybus;
+
+namespace NetCoreRabbitMqWindowsService.Handlers
+{
+    public class TestCommandHandler : ICommandHandler<TestCommand>
+    {
+        public Task HandleAsync(IDispatcher dispatcher, ICommandContext<TestCommand> incomingCommand)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
